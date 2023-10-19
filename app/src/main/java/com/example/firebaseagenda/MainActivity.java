3e4403a47e1b4708730db4fb6c7a2c146bd30731
Franciel <franciel.technix@gmail.com>
2023-10-18 23:38:42 -0300

    * Adicionado o update e o delete (sem tratamento de erros)

--- app/src/main/java/com/example/firebaseagenda/MainActivity.java
+++ app/src/main/java/com/example/firebaseagenda/MainActivity.java
@@ -15,6 +15,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -96,17 +100,14 @@ public class MainActivity extends AppCompatActivity {
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
             }
-
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
             }
-
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -114,11 +115,84 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
+    /**
+     * Este método lê um registro específico no banco de dados Firebase, identificado pelo
+     * ID fornecido, e atualiza os campos "nome" e "phone" desse registro com os valores
+     * fornecidos pelos EditTexts mEditTextName e mEditTextPhone.
+     * @param snapshot The current data at the location
+     */
     private void updateAgendaData() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        long ID = Long.parseLong(mEditTextID.getText().toString());
+        String name = mEditTextName.getText().toString();
+        String phone = mEditTextPhone.getText().toString();
+        /*
+          Realiza uma consulta no nó "agenda" do banco de dados Firebase, onde o
+          campo "id" é igual a ID (o valor obtido do mEditTextID).
+          addListenerForSingleValueEvent é usado para escutar eventos de dados uma vez.
+         */
+        reference.child("agenda").orderByChild("id").equalTo(ID).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            /**
+             * Este método é chamado quando os dados na referência são alterados.
+             * snapshot contém os dados atuais no local especificado.
+             * @param snapshot The current data at the location
+             */
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Para todos os filhos de snapshot utilizando o nome "data" do tipo DataSnapshot
+                // Espera-se que haja apenas um filho correspondente ao ID fornecido.
+                for(DataSnapshot data : snapshot.getChildren()) {
+                    Map<String,Object> valoresAtualizados = new HashMap<>();
+                    valoresAtualizados.put("nome", name);
+                    valoresAtualizados.put("telefone", phone);
+                    // É usado para realizar a atualização no banco de dados Firebase. Ele atualiza
+                    // os campos especificados no mapa valoresAtualizados para o nó
+                    // específico identificado pelo data.getRef().
+                    data.getRef().updateChildren(valoresAtualizados);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void deleteAgendaData() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        long ID = Long.parseLong(mEditTextID.getText().toString());
+
+    /*
+        Realiza uma consulta no nó "agenda" do banco de dados Firebase, onde o
+        campo "id" é igual a ID (o valor obtido do mEditTextID).
+        addListenerForSingleValueEvent é usado para escutar eventos de dados uma vez.
+    */
+        reference.child("agenda").orderByChild("id").equalTo(ID).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            /**
+             * Este método é chamado quando os dados na referência são alterados.
+             * snapshot contém os dados atuais no local especificado.
+             * @param snapshot The current data at the location
+             */
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Para todos os filhos de snapshot utilizando o nome "data" do tipo DataSnapshot
+                // Espera-se que haja apenas um filho correspondente ao ID fornecido.
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    // Remove o nó específico identificado pelo data.getRef().
+                    data.getRef().removeValue();
+                    // Limpa os campos de entrada após a exclusão.
+                    clearView();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Lida com erros, se necessário.
+            }
+        });
     }
 
     private void clearView(){
